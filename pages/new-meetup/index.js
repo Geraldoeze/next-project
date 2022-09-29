3cc3245c221bf4bf10a8d0fcc6c7af2814f37bb1
Gerald Ezenagu <egerald344@gmail.com>
2022-09-29 22:31:08 +0100

    working on form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,13 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+
+    function addMeetupHandler(enteredValue) {
+        console.log(enteredValue)
+    }
+ return (
+     <NewMeetupForm onAddMeetup={addMeetupHandler} />
+ )
+} 
+
+export default NewMeetupPage;
