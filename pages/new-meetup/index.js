00436d0ec1668d790bae474dae5a579e9b27bd7a
Gerald Ezenagu <egerald344@gmail.com>
2022-10-02 21:03:24 +0100

    Post req sent

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,17 @@ import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
 
-    function addMeetupHandler(enteredValue) {
-        console.log(enteredValue)
+    async function addMeetupHandler(enteredValue) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredValue),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });        
+
+        const data = await response.json();
+        console.log(data)
     }
  return (
      <NewMeetupForm  onAddMeetup={addMeetupHandler} />
